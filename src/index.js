b0433c9b72f63e26ef28e19d851701f8960070d9
LeeTibble <ltibble@live.com>
2026-05-24 12:47:41 +0100

    Change import of App to use TypeScript file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './theme-park-organiser';
+import App from './theme-park-organiser.tsx';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
